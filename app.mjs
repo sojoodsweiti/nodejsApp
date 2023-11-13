b5a3b9bbdf88c3cc865ec1270ba5bf89ac256d18
Sojood Sweiti <77960322+sojoodsweiti@users.noreply.github.com>
2023-11-13 14:35:43 +0200

    updates of app with json

--- app.mjs
+++ app.mjs
@@ -1,9 +1,10 @@
 // Import the 'http' and 'fs' modules
 import { createServer } from 'http';
-import { readFile } from 'fs';
+import { readFile, writeFile } from 'fs';
+
 
 const server = createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/get-json') {
+    if (req.method === 'GET' && req.url === '/') {
         readFile('data.json', 'utf8', (error, data) => {
             if (error) {
                 // If there's an error reading the file, respond with an Internal Server Error (HTTP 500)
@@ -15,12 +16,48 @@ const server = createServer((req, res) => {
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(data);
             }
-            })
-    }
-
-
-})   
-
+            });
+    }else if (req.method === 'POST' && req.url === '/') {
+        let body = '';
+      
+        req.on('data', (chunk) => {
+          body += chunk;
+        });
+      
+        req.on('end', () => {
+          try {
+            // Parse the incoming JSON data
+            const newData = JSON.parse(body);
+      
+            // Read the existing JSON file
+            readFile('data.json', 'utf8', (readError, existingData) => {
+              if (readError) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+              } else {
+                // Merge the existing data with the new data
+                const mergedData = { ...JSON.parse(existingData), ...newData };
+      
+                // Write the merged data back to the JSON file
+                writeFile('data.json', JSON.stringify(mergedData), (writeError) => {
+                  if (writeError) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Internal Server Error');
+                  } else {
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ message: 'JSON data updated successfully' }));
+                  }
+                });
+              }
+            });
+          } catch (error) {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ error: 'Invalid JSON data' }));
+          }
+        });
+      }
+       
+});    
 
 const port = 3000;
 // Start the server and listen on port 3000
---
+++ singlton.js
@@ -0,0 +1,37 @@
+class Counter {
+    constructor() {
+      this.count = 0;
+    }
+  
+    increment() {
+      this.count++;
+      console.log(`Count: ${this.count}`);
+    }
+  }
+  
+  // Create an instance of the Counter class
+  const counterInstance = new Counter();
+  
+  // Get user input using readline
+  const readline = require('readline');
+  const readLine = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readLine.question('Enter the number of times to increment: ', function (userInputCount) {
+    // Convert the user input to a number
+    const countToIncrement = parseInt(userInputCount, 10);
+  
+    // Use a loop to increment the counter
+    for (let i = 0; i < countToIncrement; i++) {
+      counterInstance.increment();
+    }
+  
+    // Close the readline interface
+    readLine.close();
+  });
+  
+  // Prevent further modifications to the prototype
+  Object.freeze(Counter.prototype);
+  
